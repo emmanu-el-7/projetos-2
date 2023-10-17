9e9e4506c8f5b0a8ae9070a2d5d86cb1657817b2
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2023-10-17 18:02:27 -0300

    commit backend pt. 2

--- instaufsc/backend/controllers/UserController.js
+++ instaufsc/backend/controllers/UserController.js
@@ -8,5 +8,47 @@ const generateToken = id => {
 }
 
 const register = async (req, res) => {
-  res.send('Registro efetuado.')
+  //res.send('Registro efetuado.')
+
+  const { name, email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user) {
+    res.status(422).json({ errors: ['Por favor, utilize outro e-mail.'] })
+  }
+
+  const salt = await bcrypt.genSalt()
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash
+  })
+
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ['Houve um erro, por favor tente mais tarde'] })
+
+    return
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id)
+  })
+}
+
+const getCurrentUser = async (req, res) => {
+  const user = req.user
+
+  res.status(200).json(user)
+}
+
+module.exports = {
+  register,
+  login,
+  getCurrentUser
 }
