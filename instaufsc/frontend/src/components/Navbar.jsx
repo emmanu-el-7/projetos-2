692d70d04c77d3182a8a097b12c9a52ae94a2fb0
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2023-10-28 21:42:50 -0300

    finalização frontend-1

--- instaufsc/frontend/src/components/Navbar.jsx
+++ instaufsc/frontend/src/components/Navbar.jsx
@@ -8,7 +8,13 @@ import {
   BsFillCameraFill
 } from 'react-icons/bs'
 
+import { useAuth } from '../hooks/useAuth'
+import { useSelector } from 'react-redux'
+
 const Navbar = () => {
+  const { auth } = useAuth()
+  const { user } = useSelector(state => state.auth)
+
   return (
     <div>
       <nav id='nav'>
@@ -18,17 +24,39 @@ const Navbar = () => {
           <input type='text' placeholder='Pesquisar' />
         </form>
         <ul id='nav-links'>
-          <li>
-            <NavLink to={'/'}>
-              <BsHouseDoorFill />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/login'}>Entrar</NavLink>
-          </li>
-          <li>
-            <NavLink to={'/register'}>Cadastrar</NavLink>
-          </li>
+          {auth ? (
+            <>
+              <li>
+                <NavLink to={'/'}>
+                  <BsHouseDoorFill />
+                </NavLink>
+              </li>
+              {user && (
+                <li>
+                  <NavLink to={`/user/${user._id}`}>
+                    <BsFillCameraFill />
+                  </NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to={'/profile'}>
+                  <BsFillPersonFill />
+                </NavLink>
+              </li>
+              <li>
+                <span>Sair</span>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to={'/login'}>Entrar</NavLink>
+              </li>
+              <li>
+                <NavLink to={'/register'}>Cadastrar</NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </div>
