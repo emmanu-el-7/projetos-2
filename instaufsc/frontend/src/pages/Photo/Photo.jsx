eda598384e96f2c19bb619c9c9fd71dce24dfc68
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2023-11-19 22:21:05 -0300

    finalização

--- instaufsc/frontend/src/pages/Photo/Photo.jsx
+++ instaufsc/frontend/src/pages/Photo/Photo.jsx
@@ -1,40 +1,43 @@
-import "./Photo.css";
+import './Photo.css'
 
-import { uploads } from "../../utils/config";
+import { uploads } from '../../utils/config'
 
-import Message from "../../components/Message";
-import PhotoItem from "../../components/PhotoItem";
-import { NavLink } from "react-router-dom";
-
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
-import { getPhoto } from "../../slices/photoSlice";
+import Message from '../../components/Message'
+import PhotoItem from '../../components/PhotoItem'
+import { NavLink } from 'react-router-dom'
+import LikeContainer from '../../components/LikeContainer'
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getPhoto } from '../../slices/photoSlice'
 
 const Photo = () => {
-  const { id } = useParams();
+  const { id } = useParams()
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const { user } = useSelector((state) => state.auth);
-  const { photo, loading, error, message } = useSelector(
-    (state) => state.photo
-  );
+  const { user } = useSelector(state => state.auth)
+  const { photo, loading, error, message } = useSelector(state => state.photo)
 
   //carregamento dos dados da foto
   useEffect(() => {
-    dispatch(getPhoto(id));
-  }, [dispatch, id]);
+    dispatch(getPhoto(id))
+  }, [dispatch, id])
+
+  const handleLike = () => {
+    dispatch(like(photo._id))
+  }
 
   if (loading) {
-    return <p>Carregando...</p>;
+    return <p>Carregando...</p>
   }
 
   return (
-    <div id="photo">
+    <div id='photo'>
       <PhotoItem photo={photo} />
+      <LikeContainer photo={photo} user={user} handlelike={handleLike} />
     </div>
-  );
-};
+  )
+}
 
-export default Photo;
+export default Photo
