a6edd020fe2cbb6d3616cde0936b10b24940a1b8
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2023-11-21 18:51:21 -0300

    instaufsc

--- instaufsc/backend/config/db.js
+++ instaufsc/backend/config/db.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
+const mongoose = require('mongoose')
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
 
 const conn = async () => {
   try {
     const dbConn = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ma1pcqp.mongodb.net/?retryWrites=true&w=majority`
-    );
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.zohwwi3.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`
+    )
 
-    console.log("Conectou-se com o banco de dados.");
+    console.log('Conectou-se com o banco de dados.')
 
-    return dbConn;
+    return dbConn
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-conn();
+conn()
 
-module.exports = conn;
+module.exports = conn
